b867c6f7c95bbe4dcfb4dbdb7aba6c8df0874d31
student-a502 <student-a502@STUD-RM3-A502>
2025-11-28 10:47:14 +0500

    Разработан интерфейс для клиента

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+
+namespace Client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary> IP сервера
+        public static IPAddress IpAdress;
+        /// <summary> Порт сервера
+        public static int Port;
+        /// <summary> Код пользователя
+        public static int id = -1;
+        public static bool CheckCommand(string message)
+        {
+            bool BCommand = false;
+            string[] DataMessage = message.Split(new string[1] { " " }, StringSplitOptions.None);
+            if (DataMessage.Length > 0)
+            {
+                string Command = DataMessage[0];
+                if (Command == "connect")
+                {
+                    if (DataMessage.Length != 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Использование: connect [login] [password]\nПример: connect User1 P@ssw0rd";
+                        BCommand = false;
+                    }
+                    else
+                    {
+                        BCommand = true;
+                    }
+                }
+                else if (Command == "cd")
+                    BCommand = true;
+                else if (Command == "get")
+                {
+                    if (DataMessage.Length == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Использование: get [NameFile]\nПример: get Test.txt");
+                        BCommand = false;
+                    }
+                    else
+                        BCommand = true;
+
+                }
+                else if (Command == "set")
+                {
+                    if (DataMessage.Length == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Использование: set [NameFile]\nПример: set Test.txt");
+                        BCommand = false;
+                    }
+                    else BCommand = true;
+                }
+                return BCommand;
+            }
+        }
+    }
+}
