923bcf8bb53ecf2efd427e99e5efe7432856cffd
student-a502 <student-a502@STUD-RM3-A502>
2025-11-28 15:24:34 +0500

    Переделал и доделал Булатов на 5

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,11 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 using Common;
 using MySql.Data.MySqlClient;
 using Newtonsoft.Json;
